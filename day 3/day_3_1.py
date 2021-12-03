cb0555a4e658627a7d650132a269a98cf05ecdcf
bverpaalen <brentverpaalen95@hotmail.com>
2021-12-03 11:38:30 +0100

    day 3 Learned how to use Python Typing

---
+++ day 3/day_3_1.py
@@ -0,0 +1,40 @@
+input_path = "input.txt"
+
+input_file = open(input_path, 'r')
+
+byte = input_file.readline()
+pos_count = {}
+while(byte):
+    for pos in range(len(byte)):
+        bit = byte[pos]
+
+        if pos in pos_count.keys():
+            pos_count[pos][int(bit)] += 1
+        else:
+
+            if bit == "0":
+                pos_count.update({pos: [1, 0]})
+            elif bit == "1":
+                pos_count.update({pos: [0, 1]})
+
+    byte = input_file.readline()
+
+gamma_byte = ""
+epsilon_byte = ""
+for pos in range(len(pos_count.keys())):
+    if pos_count[pos][0] > pos_count[pos][1]:
+        gamma_byte += "0"
+        epsilon_byte += "1"
+    elif pos_count[pos][0] < pos_count[pos][1]:
+        gamma_byte += "1"
+        epsilon_byte += "0"
+
+gamma = int(gamma_byte, 2)
+epsilon = int(epsilon_byte, 2)
+
+print(f"Gamma: {gamma}")
+print(f"Epsilon: {epsilon}")
+
+power_con = gamma * epsilon
+
+print(f"Power consumption: {power_con}")
---
+++ day 3/day_3_2.py
@@ -0,0 +1,74 @@
+import _io
+from typing import Callable
+
+
+def main(input_path = "input.txt"):
+    input_file = open(input_path, 'r')
+
+    bytes = read_bytes(input_file)
+
+    oxygen_gen_byte = bit_criteria(bytes, common_bit)
+    co2_scrub_byte = bit_criteria(bytes, uncommon_bit)
+
+    oxygen_gen = int(oxygen_gen_byte, 2)
+    co2_scrub = int(co2_scrub_byte, 2)
+
+    print(f"Oxygen: {oxygen_gen}")
+    print(f"CO2: {co2_scrub}")
+
+    life_sup_rat = oxygen_gen * co2_scrub
+
+    print(f"Life support rating: {life_sup_rat}")
+
+
+def bit_criteria(bytes: list, criteria: Callable[[list, int], int]) -> int:
+    pos = 0
+    while len(bytes) > 1:
+        c_bit = criteria(bytes, pos)
+
+        new_bytes = []
+        for byte in bytes:
+            bit = (byte)[pos]
+            if bit == c_bit:
+                new_bytes.append(byte)
+        bytes = new_bytes
+        pos += 1
+    return bytes[0]
+
+
+def bit_count(bytes_list: list, pos: int) -> dict:
+    pos_count = {0: 0, 1: 0}
+    for byte in bytes_list:
+        bit = byte[pos]
+        pos_count[int(bit)] += 1
+    return pos_count
+
+
+def common_bit(bytes_list: list, pos: int) -> str:
+    pos_count = bit_count(bytes_list, pos)
+    if pos_count[0] > pos_count[1]:
+        return "0"
+    else:
+        return "1"
+
+
+def uncommon_bit(bytes_list: list, pos: int) -> str:
+    pos_count = bit_count(bytes_list, pos)
+    if pos_count[0] <= pos_count[1]:
+        return "0"
+    else:
+        return "1"
+
+
+def read_bytes(f: _io.TextIOWrapper) -> list:
+    bytes = []
+    line = f.readline()
+    while(line):
+        bytes.append(line)
+        line = f.readline()
+    return bytes
+
+
+if __name__ == '__main__':
+    main()
+
